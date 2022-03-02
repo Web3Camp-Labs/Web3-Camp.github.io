fe2b92640ae3bb5bce301f088373d7ba2e75bb93
xrdavies <xrdavies@users.noreply.github.com>
2022-03-02 08:04:13 +0000

    deploy: 818d0648c57d700e5dc2cd8cdff423cc19f5a6c9

---
+++ _next/static/QxD_giUMetH2IkvtlK_M0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/931-8ede4eecd5b48948.js","static/chunks/pages/index-b438efd491d5eebd.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
