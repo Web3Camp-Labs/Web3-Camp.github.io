4ae521ac89610638e2d171f8b342f06ddaa136e7
wendychaung <wendychaung@users.noreply.github.com>
2022-03-04 06:40:29 +0000

    deploy: dcae33afc1e93649f0ceb4313dd547238a8a6fa8

---
+++ _next/static/uN0WfFJHsxlTDAF_jwzJd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/931-2ea9e8fdc67071a9.js","static/chunks/pages/index-5344b5596b60f6c4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
