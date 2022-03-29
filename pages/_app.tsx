2892b6371c0ada9f232f7893a5ba0958d1c26431
Frozen <1884084+xrdavies@users.noreply.github.com>
2022-03-29 20:25:37 +0800

    update for og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import { NextPage } from 'next'
-import { ReactNode,ComponentType,ReactElement } from 'react';
+import { ReactNode, ComponentType, ReactElement } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/globals.css'
@@ -20,10 +20,24 @@ type IAppProps = AppProps & {
 
 function MyApp({ Component, pageProps }: IAppProps) {
   const getLayout = Component.getLayout ?? ((page: ReactNode) => page)
-  return  <>
+  return <>
     <Head>
       <title>Web3 Camp - The camp for web3 developers</title>
       <meta name="description" content="Web3 Camp - Tools and resources for all Web 3.0 enthusiasts. Sharpen your weapon and get started with Web 3.0 from here." />
+
+      <meta property="og:url" content="https://web3camp.us/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Web3 Camp - The camp for web3 developers" />
+      <meta property="og:description" content="Web3 Camp - Tools and resources for all Web 3.0 enthusiasts. Sharpen your weapon and get started with Web 3.0 from here." />
+      <meta property="og:image" content="" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="web3camp.us" />
+      <meta property="twitter:url" content="https://web3camp.us/" />
+      <meta name="twitter:title" content="Web3 Camp - The camp for web3 developers" />
+      <meta name="twitter:description" content="Web3 Camp - Tools and resources for all Web 3.0 enthusiasts. Sharpen your weapon and get started with Web 3.0 from here." />
+      <meta name="twitter:image" content="" />
+
       <link rel="icon" href="./favicon.ico" />
     </Head>
     {getLayout(<Component {...pageProps} />)}
