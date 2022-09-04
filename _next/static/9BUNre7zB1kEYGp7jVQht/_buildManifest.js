f8e4da1f25c82f54cec1437a83520d4a30f13109
xrdavies <xrdavies@users.noreply.github.com>
2022-09-04 10:07:38 +0000

    deploy: 3f674cf3739f834237c02ec2c5bb5024ab6ff610

--- _next/static/9BUNre7zB1kEYGp7jVQht/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/931-2ea9e8fdc67071a9.js","static/chunks/pages/index-226026c20bda1fa2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/VBdAftafcAs9S1vDolvXt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/931-2ea9e8fdc67071a9.js","static/chunks/pages/index-4df51a0eb2d8a9ed.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
